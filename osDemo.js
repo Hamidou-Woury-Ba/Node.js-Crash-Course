933d840eac779bd2e10b488940d647fe0fb484a8
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-07-29 15:04:48 +0000

    Ajout du fichier osDemo.js

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os'
+
+// userInfo()
+console.log(os.userInfo().username)
+
+// totalMem()
+console.log(os.totalmem())
+
+// freeMem()
+console.log(os.freemem())
+
+// cpus
+console.log(os.cpus())
